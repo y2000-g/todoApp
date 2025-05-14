747f39f9e78b6bfc1beed16776426cdc62a1b028
Yashita Gole <yashitagole2019@gmail.com>
2025-05-14 15:39:20 +0530

    Add feature implemented

--- todo.js
+++ todo.js
@@ -1,1 +1,36 @@
 console.log("HEllo")
+
+var todoListArray = []
+
+function addTodo()
+{
+    console.log("Add to do")
+    let todoInput = document.getElementById("todoInput")
+    console.log(todoInput)
+    console.log(todoInput.value)
+    // console.log(todoListArray)
+    // todoListArray.push(todoInput.value)
+    // console.log(todoListArray)
+
+    let todolist = document.getElementById("todolist")
+    console.log(todolist)
+    console.log(todolist.innerHTML)
+    if(todoInput.value.trim() == "") // trim is not working for removing string spaces
+    {
+        alert("Todo title cannot be blank!!")
+    }
+    else
+    {
+        console.log(todoListArray)
+        todoListArray.push(todoInput.value)
+        console.log(todoListArray)
+    }
+    todolist.innerHTML = ""
+    for(index =0; index<todoListArray.length; index++)
+    {
+        console.log(index)
+        todolist.innerHTML += "<li>"+todoListArray[index]+"</li>"
+        console.log(todolist.innerHTML)
+    }
+   
+}
